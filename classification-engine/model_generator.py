d6209f395982074ff586c17cc9b7012304beb5ce
joaofelipe <joaofelipe1294@gmail.com>
2023-08-01 10:44:46 -0300

    feat: remove pandas approach and replace to an numpy only

--- classification-engine/model_generator.py
+++ classification-engine/model_generator.py
@@ -1,35 +1,13 @@
-import pandas as pd
 from keras.utils import to_categorical
-from tensorflow import keras
 from keras import Sequential
 from keras.layers import Dense, InputLayer
 from keras.initializers import RandomNormal
 from sklearn.utils import shuffle
+from utils import load_image_paths_and_labels, load_images_and_labels
 
-# load csv dataset
-def load_dataframe():
-  print('Loading data ...')
-  dataframe = pd.read_csv(
-    DATASET_PATH,
-    header=None,
-    skiprows=1
-  )
-  print('Loading data [OK]')
-
-  return dataframe
-
-# split X and y
-def split_bases(dataframe):
-  print('Split X and y ...')
-  X = dataframe.values[:,:-1]
-
-  y = dataframe.values[:, -1]
-  y = to_categorical(y)
-
-  X, y = shuffle(X, y) # without shuffle data accuracy on test was 0.7 and after shuffle it grows to 0.97
-
-  print('Split X and y [OK]')
-  return X, y
+LABELS_FILE_PATH = '../digits/train.txt'
+DATASET_PATH = '../digits/train'
+SEED = 42
 
 def build_model(X, y):
   print('Training model ...')
@@ -56,12 +34,8 @@ def build_model(X, y):
 
   return model
 
-
-DATASET_PATH = '../digits/train.csv'
-SEED = 42
-
-
-dataframe = load_dataframe()
-X, y = split_bases(dataframe)
+images_with_labels = load_image_paths_and_labels(LABELS_FILE_PATH)
+images, labels = load_images_and_labels(images_with_labels, DATASET_PATH)
+X, y = shuffle(images, labels)
 model = build_model(X, y)
 model.save('model_v1.h5')
