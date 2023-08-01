bf53dff63bb2532dc62d3631336ed48ff4961148
joaofelipe <joaofelipe1294@gmail.com>
2023-08-01 10:44:56 -0300

    refactor: use utils functions

--- classification-engine/model_validation.py
+++ classification-engine/model_validation.py
@@ -1,37 +1,13 @@
-from tensorflow import keras
-import pandas as pd
 from keras.utils import to_categorical
 from keras.models import load_model
+from utils import load_image_paths_and_labels, load_images_and_labels
 
-
-# load csv dataset
-def load_dataframe():
-  print('Loading data ...')
-  dataframe = pd.read_csv(
-    DATASET_PATH,
-    header=None,
-    skiprows=1
-  )
-  print('Loading data [OK]')
-
-  return dataframe
-
-# split X and y
-def split_bases(dataframe):
-  print('Split X and y ...')
-  X = dataframe.values[:,:-1]
-
-  y = dataframe.values[:, -1]
-  y = to_categorical(y)
-
-  print('Split X and y [OK]')
-  return X, y
-
-DATASET_PATH = '../digits/val.csv'
+LABELS_FILE_LPATH = '../digits/test.txt'
+DATASET_PATH = '../digits/test'
 MODEL_PATH = './model_v1.h5'
 
-dataframe = load_dataframe()
-X, y = split_bases(dataframe)
+images_with_labels = load_image_paths_and_labels(LABELS_FILE_LPATH)
+X, y = load_images_and_labels(images_with_labels, DATASET_PATH)
 model = load_model(MODEL_PATH)
 
 result = model.evaluate(X, y)
